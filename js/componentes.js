17dfe072db84a71d16eb0155865aeb0bbc61cd0b
fer <fecamo22@bemen3.cat>
2023-05-16 19:09:26 +0200

    Update componentes.js

--- js/componentes.js
+++ js/componentes.js
@@ -1,5 +1,5 @@
 // fetch para recoger el nav.html (menu) y cargarlo siempre que <nav> tenga de id => (contenido)
-export default nav = () => fetch('componentes/nav.html')
+ fetch('componentes/nav.html')
     .then(response => response.text())
     .then(text => document.getElementById('contenido').innerHTML = text);
 
