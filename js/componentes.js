6925f2dfba3032692a6521ff6b37afffda08053c
fer <fecamo22@bemen3.cat>
2023-05-16 19:00:44 +0200

    Revert "d"
    This reverts commit db88fa760b35d1eeb0cc7cca59fbf3c22bc53588.

--- js/componentes.js
+++ js/componentes.js
@@ -1,10 +1,6 @@
 // fetch para recoger el nav.html (menu) y cargarlo siempre que <nav> tenga de id => (contenido)
-<<<<<<< HEAD
 function componentes() {
     return fetch('../componentes/nav.html')
-=======
-fetch('componentes/nav.html')
->>>>>>> parent of f746d71 (a)
     .then(response => response.text())
     .then(text => document.getElementById('contenido').innerHTML = text);
 
